29f1d496c14705468a3a46d430d3e84e0602cfa2
asiernc <asierncara@gmail.com>
2025-06-03 09:34:14 +0200

    donee exercise 5

---
+++ 3-06/exercise5.py
@@ -0,0 +1,5 @@
+hours = float(input("Introduce tus horas mensuales: "))
+salary = float(input("Introduce el salario/hora: "))
+count = hours * salary
+
+print(f"La paga que te corresponde sube a: {count}€")
