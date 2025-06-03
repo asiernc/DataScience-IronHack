eb445e6b4c0d686e00225a4da17c9baf392e2d3e
asiernc <asierncara@gmail.com>
2025-06-03 09:51:48 +0200

    done exercise 6

--- 3-06/exercise5.py
+++ 3-06/exercise5.py
@@ -1,5 +1,23 @@
-hours = float(input("Introduce tus horas mensuales: "))
-salary = float(input("Introduce el salario/hora: "))
+while True:
+	try:
+		hours = float(input("Introduce tus horas mensuales: "))
+		if hours <= 0:
+			print("Las horas han de tener valor positivo.")
+			continue
+		break
+	except ValueError:
+		print("Por favor, introduce un numero válido.")
+
+while True:
+	try:
+		salary = float(input("Introduce el salario/hora: "))
+		if salary <= 0:
+			print("El salario ha de tener valor positivo.")
+			continue
+		break
+	except ValueError:
+		print("Por favor, introduce un numero válido.")
+
 count = hours * salary
 
 print(f"La paga que te corresponde sube a: {count}€")
---
+++ 3-06/exercise6.py
@@ -0,0 +1,5 @@
+num = int(input("Introduce un numero entero:"))
+
+sum = num * (num + 1) // 2
+
+print(f"La suma resultante de los enteros es {sum}")
